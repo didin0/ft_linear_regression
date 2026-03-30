bebef9840db2a2396d943b09f81a6a701f61747c
didin0 <mehdi.abbadi.pro@gmail.com>
2026-03-30 20:22:16 +0200

    mandatory OK | Bonus metrics ongoing

--- src/predict.cpp
+++ src/predict.cpp
@@ -1,29 +1,52 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <limits>
+#include <algorithm>
 
 int main() {
     double theta0 = 0.0;
     double theta1 = 0.0;
-    int mileage = 0.0;
+    double mileage = 0.0;
+    std::string input;
+    std::string::size_type sz;
 
-    try {
-        std::ifstream inFile("thetas.txt");
-        if (inFile.is_open()) {
-            inFile >> theta0;
-            inFile >> theta1;
-            std::cout << theta0 << " | " << theta1 << std::endl;
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "Erreur de lecture : " << e.what() << std::endl;
+    std::ifstream inFile("thetas.txt");
+    if (inFile.is_open()) {
+        inFile >> theta0;
+        inFile >> theta1;
     }
 
-    std::cout << "Enter a mileage :";
-    std::cin >> mileage;
+    while (true) {
+        std::cout << "Enter a mileage: ";
+        std::getline(std::cin, input);
+        if (input.find(',') != std::string::npos) {
+            std::replace(input.begin(), input.end(), ',', '.');
+        }
+        try {
+            mileage = std::stod(input, &sz);
+            if (sz != input.size()) {
+                std::cerr << "Erreur : Veuillez entrer uniquement des chiffres : " << std::endl;
+                continue;
+            }
+            if (mileage < 0) {
+                std::cerr << "Erreur : Veuillez entrer uniquement des positifs : " << std::endl;
+                continue;
+            }
+            break;
+        } catch (const std::exception& e) {
+            std::cerr << "Erreur : Veuillez entrer un kilometrage valide : " << e.what() << std::endl;
+        }
+    }
 
     double estimatedPrice = theta0 + (theta1 * mileage);
 
-    std::cout << "The Estimated price is :" << estimatedPrice << std::endl;
+    // Un prix de voiture ne peut pas être négatif
+    if (estimatedPrice < 0) {
+        estimatedPrice = 0;
+    }
+
+    std::cout << "The estimated price is: " << estimatedPrice << std::endl;
 
     return 0;
 }
--- src/train.cpp
+++ src/train.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <csv.h>
 #include <fstream>
+#include <algorithm>
+#include <cmath>
 
 int main() {
     std::vector<double> mileages;
@@ -25,16 +27,85 @@ int main() {
     } catch (const std::exception& e) {
         std::cerr << "Erreur de lecture : " << e.what() << std::endl;
     }
+    
+    if (mileages.empty()) {
+        std::cerr << "Erreur : aucune donnee a traiter." << std::endl;
+        return 1;
+    }
 
-    double theta0 = 5.0;
-    double theta1 = -0.5;
+    const auto [min_it, max_it] = std::minmax_element(begin(mileages), end(mileages));
+    double min_km = *min_it;
+    double max_km = *max_it;
+
+    // Normaliser les kilométrages (entre 0 et 1)
+    std::vector<double> norm_mileages;
+    for (double km : mileages) {
+        norm_mileages.push_back((km - min_km) / (max_km - min_km));
+    }
+
+    // Étape 4 : La Descente de Gradient (Le modèle mathématique du sujet)
+    double theta0 = 0.0;
+    double theta1 = 0.0;
+    double learningRate = 0.1; // Pas de 0.1 comme normalized de 0 a 1.
+    int iterations = 10000;    // Le nombre de fois qu'on répète l'apprentissage
+    int m = mileages.size();   // La taille de notre dataset ('m' dans la formule)
+
+    for (int iter = 0; iter < iterations; ++iter) {
+        double sum_error0 = 0.0;
+        double sum_error1 = 0.0;
+
+        // Calcul des erreurs
+        for (int i = 0; i < m; ++i) {
+            double estimatePrice = theta0 + (theta1 * norm_mileages[i]);
+            double error = estimatePrice - prices[i];
+            
+            sum_error0 += error;
+            sum_error1 += error * norm_mileages[i];
+        }
+
+        // Formule du sujet
+        double tmp_theta0 = learningRate * (1.0 / m) * sum_error0;
+        double tmp_theta1 = learningRate * (1.0 / m) * sum_error1;
+
+        theta0 -= tmp_theta0;
+        theta1 -= tmp_theta1;
+    }
+
+    // Dénormalisation
+    double final_theta1 = theta1 / (max_km - min_km);
+    double final_theta0 = theta0 - (final_theta1 * min_km);
 
     std::ofstream outFile("thetas.txt");
     if (outFile.is_open()) {
-        outFile << theta0 << "\n" << theta1 << "\n";
+        outFile << final_theta0 << "\n" << final_theta1 << "\n";
         outFile.close();
-        std::cout << "Thetas Sauvegardes!" << std::endl;
+        std::cout << "Entrainement termine avec succes ! Thetas sauvegardes." << std::endl;
     }
+
+    // Bonus metrics
+    // MAE (Mean Absolute Error)
+    double mae = 0.0;
+
+    for(int i = 0; i < m; ++i) {
+        double prediction = final_theta0 + (final_theta1 * mileages[i]);
+        double erreur_price_euro = std::abs(prices[i] - prediction);
+
+        mae += erreur_price_euro;
+    }
+    mae /=  m;
+
+    std::cout << "MAE : " << mae << "€ en moyenne." << std::endl;
+
+    // MAE (Mean Squared Error)
+    double mse = 0.0;
+
+    for(int i = 0; i < m; ++i) {
+        double prediction = final_theta0 + (final_theta1 * mileages[i]);
+        double erreur_au_carre = (prices[i] - prediction) * (prices[i] - prediction);
+        mse += erreur_au_carre;
+    }
+    mse /=  m;
+    std::cout << "MSE : " << mse << "€ en moyenne." << std::endl;
 
     return 0;
 }
